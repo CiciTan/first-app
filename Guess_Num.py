c88f7226b0c86b492e3cfe614befc56dba659f63
Cici Tan <cicitan0502@gmail.com>
2020-04-04 15:20:42 +0100

    Add comment

--- Guess_Num.py
+++ Guess_Num.py
@@ -2,6 +2,8 @@
 # By Tony
 
 import random
+
+
 # random is a module.
 # A module in Python include functions, constants, classes (with fields and methods encapsulated inside)
 # First import the module first.
@@ -12,47 +14,57 @@ import random
 # Now you can directly call the seed function:
 # 	seed()
 
-def Validate_Continue() -> bool:
-# Varify whether the player wants to continue. Filter out invalid responses.
+# Verify whether the player wants to continue. Filter out invalid responses.
 # Return True or False only
-	while(True):
-		ans = input("Do you want to continue? Y/N \n")
-		if (ans == 'n') or (ans == 'N') or (ans == "no") or (ans == "No") or (ans == "NO") or (ans == "Nope") or (ans == "nope"): return False
-		elif (ans == 'y') or (ans == 'Y') or (ans == "yes") or (ans == "Yes") or (ans == "YES") or (ans == "Yeah") or (ans == "yeah"): return True
-		else: print("Answer not valid! Please try again.")
+def validate_continue() -> bool:
+    while True:
+        ans = input("Do you want to continue? Y/N \n")
+        if (ans == 'n') or (ans == 'N') or (ans == "no") or (ans == "No") or (ans == "NO") or (ans == "Nope") or (
+                ans == "nope"):
+            return False
+        elif (ans == 'y') or (ans == 'Y') or (ans == "yes") or (ans == "Yes") or (ans == "YES") or (ans == "Yeah") or (
+                ans == "yeah"):
+            return True
+        else:
+            print("Answer not valid! Please try again.")
 
-def Input_Num() -> int:
 # Convert user input into int. Filter out invalid input.
 # Return int
-	while(True):
-		try:
-			keyin = int(input("Please guess an integer between 0 to 3: \n"))
-			return keyin
+def input_num() -> int:
+    while True:
+        try:
+            keyin = int(input("Please guess an integer between 0 to 3: \n"))
+            return keyin
 
-		except ValueError:
-			print("Invalid input! Please try again.")
+        except ValueError:
+            print("Invalid input! Please try again.")
 
-def Gen_Random_Int() -> int:
+
 # Generate a random integer
 # Return int
-	random.seed()
-	# Seed is required to generate random numbers
-	return int(random.random() * 3)
-	# random.random() generates a random number between 0 to 1
+def gen_random_int() -> int:
+    random.seed()
+    # Seed is required to generate random numbers
+    return int(random.random() * 3)
 
-def Win_Or_Lose(Random_Int, Input_Num):
+
+# random.random() generates a random number between 0 to 1
+
 # Game logic, validate whether user guessed correctly
 # No return
-	if Random_Int == Input_Num: print("Yeah, you guessed right!")
-	else: print("Nope, wrong guess. My number is actually: ", Random_Int)
+def win_or_lose(random_int, input_num):
+    if random_int == input_num:
+        print("Yeah, you guessed right!")
+    else:
+        print("Nope, wrong guess. My number is actually: ", random_int)
 
 
 # Main thread is here
-while(True):
-	
-	Win_Or_Lose(Gen_Random_Int(), Input_Num())
-	
-	if Validate_Continue() == False: break
-	
-print("Thanks for playing")
+while True:
 
+    win_or_lose(gen_random_int(), input_num())
+
+    if not validate_continue():
+        break
+
+print("Thanks for playing")
